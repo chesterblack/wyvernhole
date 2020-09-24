3d284d65d1753af1ee3f2dfbc7d0d77fb003df81
Chester Black <chesterblack@Chesters-MacBook-Pro.local>
2020-09-24 17:48:04 +0100

    switched database over to mongodb

--- game.php
+++ game.php
@@ -1,43 +1,23 @@
 <?php
-
+    error_reporting(E_ALL);
     include($_SERVER['DOCUMENT_ROOT'] . '/vendor/autoload.php');
-    $dotenv = Dotenv\Dotenv::createImmutable($_SERVER['DOCUMENT_ROOT']);
-    $dotenv->load();
+    
+    switch ($_GET['function']) {
+        case 'fetchRoom':
+            $collection = (new MongoDB\Client)->game->rooms;
+            $id = intval(htmlentities($_GET['room']));
+            break;
+        case 'fetchItem':
+            $collection = (new MongoDB\Client)->game->items;
+            $id = intval(htmlentities($_GET['item']));
+            break;
+    }
 
-    function connect() {
-        $dsn = "mysql:host=".$_ENV['DB_HOST'].";dbname=".$_ENV['DB_NAME'].";charset=utf8mb4";
-        $options = [
-            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-            PDO::ATTR_EMULATE_PREPARES => false,
-        ];
+    if ($collection) {
         try {
-            return $pdo = new PDO($dsn, $_ENV['DB_USER'], $_ENV['DB_PASS'], $options);
-        } catch (\PDOException $e) {
-            throw new \PDOException($e->getMessage(), (int)$e->getCode());
-        }
-    }
-
-    /**
-     * 
-     * Fetches data from rooms table
-     * 
-     * @param String $roomID - the id of the room row
-     * @return String - JSON encoded object
-     */
-    function fetchRoom($roomID){
-        $stmt = connect()->prepare('SELECT `data` FROM rooms WHERE id = :id');
-        $stmt->execute(['id' => $roomID]);
-        $results = $stmt->fetch();
-        echo $results['data'];
-    }
-
-    if (isset($_GET['function'])) {
-        switch($_GET['function']){
-            case 'fetchRoom':
-                fetchRoom($_GET['room']);
-                break;
-            default:
-                echo "Invalid function provided";
+            $result = $collection->findOne(['id' => $id]);
+            echo json_encode($result);
+        } catch (Throwable $t) {
+            echo $t->getMessage();
         }
     }
