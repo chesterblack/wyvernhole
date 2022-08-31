bdedea7a44875f6b0a1d0c851b405ad171d7dcc5
chesterblack <chez.black@hotmail.co.uk>
2022-08-31 16:58:54 +0100

    Added save game functionality, game saves when it is loaded

--- app/components/Menu.jsx
+++ app/components/Menu.jsx
@@ -30,12 +30,58 @@ export default function MenuSidebar() {
 }
 
 function OptionsMenu(props) {
-    const {
-        saveCode,
-        textSpeed,
-        setTextSpeed
-    } = useSaveContext();
+    return (
+        <>
+            <TextSpeed />
+            <SaveGame />
+            <LoadGame />
+            {/* <Button label='New Game' onClick={() => {
+                console.log('new game!');
+            }} /> */}
+            <AutosaveToggle />
+        </>
+    )
+}
+
+function TextSpeed() {
+    const { textSpeed, setTextSpeed } = useSaveContext();
+
+    return (
+        <div>
+            Text Speed:
+            <input
+                type='number'
+                id='text-speed'
+                defaultValue={textSpeed}
+                onChange={(e) => {
+                    setTextSpeed(e.target.value);
+                }}
+            />
+        </div>
+    );
+}
+
+function SaveGame() {
+    const { saveCode } = useSaveContext();
+
+    return (
+        <div>
+            Save game:
+            <input id='save-game' type='text' readOnly={true} value={saveCode} />
+            <Button label='Copy' onClick={(e) => {
+                navigator.clipboard.writeText(saveCode);
+                pingUpdateMessage(
+                    e.target,
+                    'Copied to clipboard'
+                );
+            }} />
+        </div>
+    )
+}
+
+function LoadGame() {
     const { statsChangeHandler } = useStatsContext();
+    const { saveGame } = useSaveContext();
 
     function loadGame(code) {
         let decodedOptions = JSON.parse(atob(code));
@@ -51,45 +97,19 @@ function OptionsMenu(props) {
             const value = stats[key];
             statsChangeHandler(key, value);
         }
+
+        saveGame();
     }
 
     return (
-        <>
-            <div>
-                Text Speed:
-                <input
-                    type='number'
-                    id='text-speed'
-                    defaultValue={textSpeed}
-                    onChange={(e) => {
-                        setTextSpeed(e.target.value);
-                    }}
-                />
-            </div>
-            <div>
-                Save game:
-                <input id='save-game' type='text' readOnly={true} value={saveCode} />
-                <Button label='Copy' onClick={(e) => {
-                    navigator.clipboard.writeText(saveCode);
-                    pingUpdateMessage(
-                        e.target,
-                        'Copied to clipboard'
-                    );
-                }} />
-            </div>
-            <div>
-                Load game:
-                <input id='load-game' type='text' />
-                <Button label='Load' onClick={() => {
-                    let loadCode = document.querySelector('#load-game').value;
-                    loadGame(loadCode);
-                }} />
-            </div>
-            {/* <Button label='New Game' onClick={() => {
-                console.log('new game!');
-            }} /> */}
-            <AutosaveToggle />
-        </>
+        <div>
+            Load game:
+            <input id='load-game' type='text' />
+            <Button label='Load' onClick={() => {
+                let loadCode = document.querySelector('#load-game').value;
+                loadGame(loadCode);
+            }} />
+        </div>
     )
 }
 
--- app/contexts/save-context.jsx
+++ app/contexts/save-context.jsx
@@ -4,25 +4,6 @@ import { useInventoryContext } from './inventory-context';
 
 const SaveContext = createContext();
 
-export function saveGame() {
-    const { textSpeed, setSaveCode } = useSaveContext();
-    const { stats } = useStatsContext();
-    const { inventory, equipped } = useInventoryContext().inventory;
-
-    const saveCode = btoa(JSON.stringify({
-        r: 1,
-        h: stats.health,
-        mh: stats.maxHealth,
-        g: stats.gold,
-        s: textSpeed,
-        d: stats.drunkenness,
-        i: inventory,
-        e: equipped,
-    }));
-
-    setSaveCode(saveCode);
-}
-
 export function SaveWrapper({ children }) {
     const [textSpeed, setTextSpeed] = useState(10);
     const { stats } = useStatsContext();
@@ -41,6 +22,21 @@ export function SaveWrapper({ children }) {
 
     const [saveCode, setSaveCode] = useState(defaultSaveCode);
 
+    function saveGame() {
+        const saveCode = btoa(JSON.stringify({
+            r: 1,
+            h: stats.health,
+            mh: stats.maxHealth,
+            g: stats.gold,
+            s: textSpeed,
+            d: stats.drunkenness,
+            i: inventory,
+            e: equipped,
+        }));
+    
+        setSaveCode(saveCode);
+    }
+
     return (
         <SaveContext.Provider
             value={{
@@ -48,6 +44,7 @@ export function SaveWrapper({ children }) {
                 setSaveCode,
                 textSpeed,
                 setTextSpeed,
+                saveGame,
             }}
         >
             {children}
